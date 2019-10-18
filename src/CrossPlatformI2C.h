0438b1b20fb74ba509e7547a1616dfa9e2871045
Levy <levys@wlu.edu>
2019-10-18 13:32:37 -0400

    Fixed typo in license comments

--- src/CrossPlatformI2C.h
+++ src/CrossPlatformI2C.h
@@ -1,19 +1,19 @@
 /* 
    CrossPlatformI2C.h: Cross-platform function declarations
 
-   This file is part of EM7180.
+   This file is part of CrossPlatformDataBus.
 
-   EM7180 is free software: you can redistribute it and/or modify
+   CrossPlatformDataBus is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   EM7180 is distributed in the hope that it will be useful,
+   CrossPlatformDataBus is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
    You should have received a copy of the GNU General Public License
-   along with EM7180.  If not, see <http://www.gnu.org/licenses/>.
+   along with CrossPlatformDataBus.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
