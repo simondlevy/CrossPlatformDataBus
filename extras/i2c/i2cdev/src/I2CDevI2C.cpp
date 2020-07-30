0ada74a144bada1c4c3684342b45a704498d8d06
simondlevy <simon.d.levy@gmail.com>
2020-07-30 16:39:13 -0400

    Fixed bug in cpi2c_readRegisters()

--- extras/i2c/i2cdev/src/I2CDevI2C.cpp
+++ extras/i2c/i2cdev/src/I2CDevI2C.cpp
@@ -49,32 +49,31 @@ uint8_t cpi2c_open(uint8_t address, uint8_t bus)
     return fd;
 }
 
-void cpi2c_close(uint8_t address)
+void cpi2c_close(uint8_t device)
 {
-    close(address);
+    close(device);
 }
 
-bool cpi2c_writeRegister(uint8_t address, uint8_t subAddress, uint8_t data)
+bool cpi2c_writeRegister(uint8_t device, uint8_t subAddress, uint8_t data)
 {
-    return i2c_smbus_write_byte_data(address, subAddress, data) == 0;
+    return i2c_smbus_write_byte_data(device, subAddress, data) == 0;
 }
 
-void cpi2c_readRegisters(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
+void cpi2c_readRegisters(uint8_t device, uint8_t subAddress, uint8_t count, uint8_t * dest)
 {
-    i2c_smbus_write_byte(address, subAddress);
-
     for (uint8_t k=0; k<count; ++k) {
-        dest[k] = i2c_smbus_read_byte(address);
+        i2c_smbus_write_byte(device, subAddress+k);
+        dest[k] = i2c_smbus_read_byte(device);
     }
 }
 
-bool cpi2c_writeRegister_16_8(uint8_t address, uint16_t subAddress, uint8_t data)
+bool cpi2c_writeRegister_16_8(uint8_t device, uint16_t subAddress, uint8_t data)
 {
-    return i2c_smbus_write_byte_data(address, subAddress, data) == 0;
+    return i2c_smbus_write_byte_data(device, subAddress, data) == 0;
 }
 
-uint16_t cpi2c_readRegister_8_16(uint8_t address, uint8_t subAddress)
+uint16_t cpi2c_readRegister_8_16(uint8_t device, uint8_t subAddress)
 {
-    i2c_smbus_write_byte(address, subAddress);
-    return i2c_smbus_read_word_data(address, subAddress);
+    i2c_smbus_write_byte(device, subAddress);
+    return i2c_smbus_read_word_data(device, subAddress);
 }
