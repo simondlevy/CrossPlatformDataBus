c88b367d21f957613715b3865929942c0a66ab8a
simondlevy <simon.d.levy@gmail.com>
2020-08-04 01:27:12 -0400

    added cpi2c_writeRegisters()

--- extras/i2c/wiringpi/src/WiringPiI2C.cpp
+++ extras/i2c/wiringpi/src/WiringPiI2C.cpp
@@ -27,21 +27,22 @@ uint8_t cpi2c_open(uint8_t address, uint8_t bus)
     return (uint8_t)wiringPiI2CSetup(address);
 }
 
-bool cpi2c_writeRegister(uint8_t address, uint8_t subAddress, uint8_t data)
-{
-	return wiringPiI2CWriteReg8(address, subAddress, data) == 0;
-}
 
 uint16_t cpi2c_readRegister_8_16(uint8_t address, uint8_t subAddress)
 {
     return wiringPiI2CReadReg16 (address, subAddress);
 }
 
-void cpi2c_readRegisters(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
+void cpi2c_readRegisters(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dst)
 {
-    for (uint8_t i=0; i<count; ++i) {
-        dest[i] = wiringPiI2CReadReg8(address, subAddress+i);
+    for (uint8_t k=0; k<count; ++k) {
+        dst[k] = wiringPiI2CReadReg8(address, subAddress+k);
     }
+}
+
+bool cpi2c_writeRegister(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+	return wiringPiI2CWriteReg8(address, subAddress, data) == 0;
 }
 
 bool cpi2c_writeRegister_16_8(uint8_t address, uint16_t subAddress, uint8_t data)
@@ -52,5 +53,11 @@ bool cpi2c_writeRegister_16_8(uint8_t address, uint16_t subAddress, uint8_t data
 
 bool cpi2c_writeRegisters(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * src)
 {
-    return false;
+    for (uint8_t k=0; k<count; ++k) {
+        if (!cpi2c_writeRegister(address, subAddress+k, src[k])) {
+            return false;
+        }
+    }
+
+    return true;
 }
